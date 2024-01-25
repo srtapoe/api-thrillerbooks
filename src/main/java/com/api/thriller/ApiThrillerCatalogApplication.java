bc8b815e9ca7a6a21d754b66051252e2c135e07f
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-01-25 14:24:50 -0300

    first commit

---
+++ src/main/java/com/api/thriller/ApiThrillerCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.api.thriller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiThrillerCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiThrillerCatalogApplication.class, args);
+	}
+
+}
